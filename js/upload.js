3887ebaf67c391b6b1b2dafa1faa69594ae9f9fd
Sanchez <api1901@mail.ru>
2024-12-17 10:54:22 +0300

    Add resize event for upload window

--- js/upload.js
+++ js/upload.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
     $('.downloadButton').on("click",function(){
-        let width = $(window).width();
-        let height = $(window).height();
-        $('.shadow').css("display","block");
-        $('.shadow').width(width);
-        $('.shadow').height(height);
-        $('.window').css("display","block");
-        let x = width/2 - 200;
-        $('.window').css("left",x+"px");
+         windowShow();
     });
     $('.windowClose').on("click",function(){
         $('.window').fadeOut(200);
-
         $('.shadow').fadeOut(300);
-
     });
-
 });
+$(window).resize(function(){
+    windowShow();
+});
+function windowShow(){
+    let width = $(window).width();
+    let height = $(window).height();
+    $('.shadow').css("display","block");
+    $('.shadow').width(width);
+    $('.shadow').height(height);
+    $('.window').css("display","block");
+    let x = width/2 - $('.window').width()/2;
+    let y = height/2 - 100;
+    $('.window').css("left",x+"px");
+    $('.window').css("top",y+"px");
+
+}
